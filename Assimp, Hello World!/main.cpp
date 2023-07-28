fdbb18478ce98ef7e66146d74ea58cfbf8db68e4
lucaridd <gianlucadistefano1998@gmail.com>
2023-07-28 13:02:42 +0200

    aggiunto tesseract(correggere coord pianeti inters

--- Assimp, Hello World!/main.cpp
+++ Assimp, Hello World!/main.cpp
@@ -34,6 +34,7 @@ ISoundSource* interstellarTheme = SoundEngine->addSoundSourceFromFile("resources
 void carica_universo(GLFWwindow* window);
 void carica_futurama(GLFWwindow* window);
 void carica_interstellar(GLFWwindow* window);
+void carica_tesseract(GLFWwindow* window);
 
 bool futurama_caricato = false;
 bool interstellar_caricato = false;
@@ -81,7 +82,7 @@ struct SphereCollision
     float          radius;
 }spaceshipSphere, soleSphere, mercurioSphere, venereSphere, terraSphere, 
 marteSphere, gioveSphere, saturnoSphere, uranoSphere, nettunoSphere, gargantuaSphere,mannSphere,millerSphere,
-portalUniversoSphere, portalFuturamaSphere, portalInterstellarSphere;
+portalUniversoSphere, portalFuturamaSphere, portalInterstellarSphere, tesseractSphere;
 
 bool collisionTest(SphereCollision& sfera1, const SphereCollision& sfera2) {
     glm::vec3 distanzaCentri(sfera2.centre - sfera1.centre);
@@ -1217,13 +1218,13 @@ void carica_interstellar(GLFWwindow* window) {
         glm::mat4 modelMann = glm::mat4(1.0f);
         modelMann = glm::translate(modelMann, glm::vec3(0.0f, 20.0f, 200.0f));
         modelMann = glm::scale(modelMann, glm::vec3(9.4f / 500.0f));
-        mannSphere = { glm::vec3(0.0f, 20.0f, 200.0f), 5.0f };
+        mannSphere = { glm::vec3(0.0f, 20.0f, 200.0f), 7.0f };
         shaderGeometryPass.setMat4("model", modelMann);
         mann.Draw(shaderGeometryPass);
 
         glm::mat4 modelMiller = glm::mat4(1.0f);
         modelMiller = glm::translate(modelMiller, glm::vec3(0.0f, 20.0f, 250.0f));
-        modelMiller = glm::scale(modelMiller, glm::vec3(8.6f / 5000.0f));
+        modelMiller = glm::scale(modelMiller, glm::vec3(8.6f / 1000.0f));
         millerSphere = { glm::vec3(0.0f, 20.0f, 250.0f), 5.0f };
         shaderGeometryPass.setMat4("model", modelMiller);
         miller.Draw(shaderGeometryPass);
@@ -1260,6 +1261,7 @@ void carica_interstellar(GLFWwindow* window) {
             cameraCollided = true;
             std::string Title = "Gargantua";
             RenderText(Title.c_str(), 900.0f, (float)SCR_HEIGHT / 5.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+            //carica_tesseract(window);
         }
 
         bool collisioneMann = collisionTest(spaceshipSphere, mannSphere);
@@ -1399,6 +1401,282 @@ void carica_interstellar(GLFWwindow* window) {
 
 }
 
+void carica_tesseract(GLFWwindow* window) {
+    // build and compile shaders
+    Shader shaderGeometryPass("g_buffer.vs", "g_buffer.fs");
+    Shader shaderLightingPass("deferred_shading.vs", "deferred_shading.fs");
+    Shader shaderLightBox("deferred_light_box.vs", "deferred_light_box.fs");
+    Shader skyboxShader("skybox.vs", "skybox.fs");
+
+    // load models
+    Model spaceShuttle("resources/objects/interstellar/spaceship/rocket.obj");
+    std::vector<glm::vec3> objectPositions;
+    objectPositions.push_back(glm::vec3(-3.0, -0.5, -3.0));
+    //COMMENTARE PER FARE PROVE SU UN OGGETTO APPENA CREATO(SOSTITUSCE IL SOLE)
+    //Model sole("resources/objects/universo/planets/sole/sole.obj");
+    //DECOMMENTARE PER FARE PROVE SU UN OGGETTO APPENA CREATO (SOSTITUSCE IL SOLE)
+    Model tesseract("resources/objects/interstellar/planets/tesseract/scene.gltf");
+
+    Model portalFuturama("resources/objects/portal/portal.obj");
+    Model portalUniverso("resources/objects/portal/portal.obj");
+    Model portalInterstellar("resources/objects/portal/portal.obj");
+
+    //SoundEngine->stopAllSounds();
+    //ISound* ambientSound = SoundEngine->play2D(interstellarTheme, true);
+
+    // configure g-buffer framebuffer
+// ------------------------------
+    unsigned int gBuffer;
+    glGenFramebuffers(1, &gBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+    unsigned int gPosition, gNormal, gAlbedoSpec;
+    // position color buffer
+    glGenTextures(1, &gPosition);
+    glBindTexture(GL_TEXTURE_2D, gPosition);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
+    // normal color buffer
+    glGenTextures(1, &gNormal);
+    glBindTexture(GL_TEXTURE_2D, gNormal);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
+    // color + specular color buffer
+    glGenTextures(1, &gAlbedoSpec);
+    glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpec, 0);
+    // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering 
+    unsigned int attachments[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
+    glDrawBuffers(3, attachments);
+    // create and attach depth buffer (renderbuffer)
+    unsigned int rboDepth;
+    glGenRenderbuffers(1, &rboDepth);
+    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+    // finally check if framebuffer is complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // lighting info
+    // -------------
+    const unsigned int NR_LIGHTS = 8;
+    std::vector<glm::vec3> lightPositions;
+    std::vector<glm::vec3> lightColors;
+    srand(100);
+    for (unsigned int i = 0; i < NR_LIGHTS; i++)
+    {
+        // calculate slightly random offsets
+        float xPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        float yPos = ((rand() % 100) / 100.0) * 6.0 - 4.0;
+        float zPos = ((rand() % 100) / 100.0) * 6.0 - 3.0;
+        lightPositions.push_back(glm::vec3(100, 100, 100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(-100, -100, -100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(-100, 100, 100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(100, -100, 100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(100, 100, -100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(-100, 100, -100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(100, -100, -100)); //prova posizione sole 
+        lightPositions.push_back(glm::vec3(-100, -100, 100)); //prova posizione sole 
+        // also calculate random color
+        float rColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        float gColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        float bColor = ((rand() % 100) / 200.0f) + 0.5; // between 0.5 and 1.0
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+        lightColors.push_back(glm::vec3(1.0, 1.0, 1.0));
+    }
+
+    // shader configuration
+    // --------------------
+    shaderLightingPass.use();
+    shaderLightingPass.setInt("gPosition", 0);
+    shaderLightingPass.setInt("gNormal", 1);
+    shaderLightingPass.setInt("gAlbedoSpec", 2);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        std::string Velocity = "speed:" + std::to_string((int)camera.MovementSpeed * 1000) + " km/h";
+        RenderText(Velocity.c_str(), 15.0f, (float)SCR_HEIGHT / 10.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+
+
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // 1. geometry pass: render scene's geometry/color data into gbuffer
+        // -----------------------------------------------------------------
+        // -----------------------------------------------------------------
+        glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100000.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+
+        //draw space shuttle
+        glm::mat4 modelSpaceShuttle = glm::mat4(1.0f);
+        shaderGeometryPass.use();
+        shaderGeometryPass.setMat4("projection", projection);
+        shaderGeometryPass.setMat4("view", view);
+        modelSpaceShuttle = glm::mat4(1.0f);
+        modelSpaceShuttle = glm::translate(modelSpaceShuttle, camera.Position + 0.000002f * camera.Front);
+        modelSpaceShuttle = glm::rotate(modelSpaceShuttle, glm::radians(camera.Yaw), glm::vec3(0.0f, -1.0f, 0.0f));
+        modelSpaceShuttle = glm::scale(modelSpaceShuttle, glm::vec3(0.001f));
+        spaceshipSphere = { camera.Position + 2.0f * camera.Front, 5.0f };
+        shaderGeometryPass.setMat4("model", modelSpaceShuttle);
+        spaceShuttle.Draw(shaderGeometryPass);
+
+        // draw solar system
+        glm::mat4 modelTessract = glm::mat4(1.0f);
+        modelTessract = glm::scale(modelTessract, glm::vec3(5.0f));
+        modelTessract = glm::rotate(modelTessract, 200.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        tesseractSphere = { glm::vec3(0.0f, 0.0f, 0.0f), 100.0f };
+        shaderGeometryPass.setMat4("model", modelTessract);
+        tesseract.Draw(shaderGeometryPass);
+
+        //draw portal
+        glm::mat4 modelPortalFuturama = glm::mat4(1.0f);
+        modelPortalFuturama = glm::translate(modelPortalFuturama, glm::vec3(200.0f, 0.0f, 100.0f));
+        modelPortalFuturama = glm::rotate(modelPortalFuturama, 90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        modelPortalFuturama = glm::scale(modelPortalFuturama, glm::vec3(15.7f));
+        portalFuturamaSphere = { glm::vec3(200.0f, 0.0f, 100.0f), 10.0f };
+        shaderGeometryPass.setMat4("model", modelPortalFuturama);
+        portalFuturama.Draw(shaderGeometryPass);
+
+        glm::mat4 modelPortalUniverso = glm::mat4(1.0f);
+        modelPortalUniverso = glm::translate(modelPortalUniverso, glm::vec3(-200.0f, 0.0f, 100.0f));
+        modelPortalUniverso = glm::rotate(modelPortalUniverso, 90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        modelPortalUniverso = glm::scale(modelPortalUniverso, glm::vec3(15.7f));
+        portalUniversoSphere = { glm::vec3(-200.0f, 0.0f, 100.0f), 10.0f };
+        shaderGeometryPass.setMat4("model", modelPortalUniverso);
+        portalUniverso.Draw(shaderGeometryPass);
+
+        glm::mat4 modelPortalInterstellar = glm::mat4(1.0f);
+        modelPortalInterstellar = glm::translate(modelPortalInterstellar, glm::vec3(-200.0f, 0.0f, 100.0f));
+        modelPortalUniverso = glm::rotate(modelPortalInterstellar, 90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        modelPortalInterstellar = glm::scale(modelPortalInterstellar, glm::vec3(15.7f));
+        portalInterstellarSphere = { glm::vec3(-200.0f, 0.0f, 100.0f), 10.0f };
+        shaderGeometryPass.setMat4("model", modelPortalInterstellar);
+        portalInterstellar.Draw(shaderGeometryPass);
+
+        //collisioni
+        cameraCollided = false;
+
+        bool collisionePortalFuturama = collisionTest(spaceshipSphere, portalFuturamaSphere);
+        if (collisionePortalFuturama == true) {
+            std::string Title = "TELETRANSPORT TO FUTURAMA";
+            RenderText(Title.c_str(), (float)SCR_WIDTH / 2.0f, (float)SCR_HEIGHT / 2.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+            carica_futurama(window);
+        }
+
+        bool collisionePortalUniverso = collisionTest(spaceshipSphere, portalUniversoSphere);
+        if (collisionePortalUniverso == true) {
+            std::string Title = "TELETRANSPORT TO UNIVERSE";
+            RenderText(Title.c_str(), (float)SCR_WIDTH / 2.0f, (float)SCR_HEIGHT / 2.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+            carica_universo(window);
+        }
+
+        bool collisionePortalInterstellar = collisionTest(spaceshipSphere, portalInterstellarSphere);
+        if (collisionePortalInterstellar == true) {
+            std::string Title = "TELETRANSPORT TO INTERSTELLAR";
+            RenderText(Title.c_str(), (float)SCR_WIDTH / 2.0f, (float)SCR_HEIGHT / 2.0f, 1.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+            carica_interstellar(window);
+        }
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        // 2. lighting pass: calculate lighting by iterating over a screen filled quad pixel-by-pixel using the gbuffer's content.
+        // -----------------------------------------------------------------------------------------------------------------------
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        shaderLightingPass.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, gPosition);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, gNormal);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
+        // send light relevant uniforms
+        for (unsigned int i = 0; i < lightPositions.size(); i++)
+        {
+            shaderLightingPass.setVec3("lights[" + std::to_string(i) + "].Position", lightPositions[i]);
+            shaderLightingPass.setVec3("lights[" + std::to_string(i) + "].Color", lightColors[i]);
+            // update attenuation parameters and calculate radius
+            const float constant = 1.0; // note that we don't send this to the shader, we assume it is always 1.0 (in our case)
+            const float linear = 0.7;
+            const float quadratic = 1.8;
+            shaderLightingPass.setFloat("lights[" + std::to_string(i) + "].Linear", linear);
+            shaderLightingPass.setFloat("lights[" + std::to_string(i) + "].Quadratic", quadratic);
+            // then calculate radius of light volume/sphere
+            const float maxBrightness = std::fmaxf(std::fmaxf(lightColors[i].r, lightColors[i].g), lightColors[i].b);
+            float radius = (-linear + std::sqrt(linear * linear - 4 * quadratic * (constant - (256.0f / 5.0f) * maxBrightness))) / (2.0f * quadratic);
+            shaderLightingPass.setFloat("lights[" + std::to_string(i) + "].Radius", radius);
+        }
+        shaderLightingPass.setVec3("viewPos", camera.Position);
+        // finally render quad
+        renderQuad();
+
+        // 2.5. copy content of geometry's depth buffer to default framebuffer's depth buffer
+        // ----------------------------------------------------------------------------------
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, gBuffer);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); // write to default framebuffer
+        // blit to default framebuffer. Note that this may or may not work as the internal formats of both the FBO and default framebuffer have to match.
+        // the internal formats are implementation defined. This works on all of my systems, but if it doesn't on yours you'll likely have to write to the 		
+        // depth buffer in another shader stage (or somehow see to match the default framebuffer's internal format with the FBO's internal format).
+        glBlitFramebuffer(0, 0, SCR_WIDTH, SCR_HEIGHT, 0, 0, SCR_WIDTH, SCR_HEIGHT, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+        // 3. render lights on top of scene eliminare questa parte per togliere i cubi luminosi e lasciare solo la luce
+        // --------------------------------
+        /*
+        shaderLightBox.use();
+        shaderLightBox.setMat4("projection", projection);
+        shaderLightBox.setMat4("view", view);
+        for (unsigned int i = 0; i < lightPositions.size(); i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, lightPositions[i]);
+            model = glm::scale(model, glm::vec3(10.125f));
+            shaderLightBox.setMat4("model", model);
+            shaderLightBox.setVec3("lightColor", lightColors[i]);
+            //renderCube();
+            renderSphere();
+        }
+        */
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+}
 
 int main()
 {
